46e3df7870188fa5a010b9b0b230ecd8a87c075e
Benjamin Torfu <btorfu@proton.me>
2024-08-01 12:27:03 +0000

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+Concatenation annotated method module
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    method that concatenates two strings
+    arg1: str1 - first string
+    arg2: str2 - second string
+
+    returns a concatenated string
+    '''
+    return str1 + str2
