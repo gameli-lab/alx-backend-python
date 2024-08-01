bb40be89de9c1ddb5a7ca57a783b482027c2f891
Benjamin Torfu <btorfu@proton.me>
2024-08-01 20:41:10 +0000

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+sum list annotated method module
+'''
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    sum_list method which takes input_list as argument
+    returns the sum of the list as float
+    '''
+    return sum(input_list)
