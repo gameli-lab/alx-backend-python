5ce6e06b944aa603bb689714c7d8e70e64ec89cd
Benjamin Torfu <btorfu@proton.me>
2024-08-01 19:37:00 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Floor annotated method module
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    floor method which takes a float
+    returns an integer
+    '''
+    return int(n)
