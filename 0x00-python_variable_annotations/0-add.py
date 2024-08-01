a550f98cdb3ce2f25f6a9dbe1bd333a3f23864e7
Benjamin Torfu <btorfu@proton.me>
2024-08-01 12:16:47 +0000

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Function annotation task module '''
+
+def add(a: float, b: float) -> float:
+    ''' 
+    addition method annotated 
+    return the sum of a and b as float
+    '''
+    return a + b
