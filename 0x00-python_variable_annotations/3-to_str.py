12ea8d8f4acf37e791570f459382a44df0ee3b69
Benjamin Torfu <btorfu@proton.me>
2024-08-01 19:56:54 +0000

    str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+To_string method module
+'''
+
+
+def to_string(n: float) -> str:
+    '''
+    to string method which takes float arg
+    return string
+    '''
+    return (str)n
